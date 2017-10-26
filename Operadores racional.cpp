9ec839992efd764c7d01faf2e3e34a8e4417ea2f
ElydeAngel <31499376+ElydeAngel@users.noreply.github.com>
2017-10-26 11:42:20 -0500

    Tarea 16
    Tercer ejemplo que pertenece al tema “Operadores en C”  del apartado  “Operadores racionales “

---
+++ Operadores racional.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int var1=5;
+	int var2=2;
+	int resultado=0;
+	
+	//Igual a...
+	resultado=var1 == var2;
+	printf("%d == %d = %d \n", var1, var2, resultado);
+	
+	//menor igual que...
+	resultado=var1 <= var2;
+	printf("%d <= %d = %d \n", var1, var2, resultado);
+	
+	//mayor que...
+	resultado=var1 > var2;
+	printf("%d > %d = %d \n", var1, var2, resultado);
+	
+	//Diferente de...
+	resultado=var1 != var2;
+	printf("%d != %d = %d \n", var1, var2, resultado);
+		
+	return 0;
+}
