92aae036e0e95baa454b725426170fdb89ccaf06
ElydeAngel <31499376+ElydeAngel@users.noreply.github.com>
2017-11-13 23:25:43 -0600

    Tarea 21
    Programa donde por medio de funciones se imprimen las letras de nuestro nombre

---
+++ Tarea 21/Nombre.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "Nombre.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+ 	appInfoData("Nombre con uso de funciones", "Noviembre 13, 2017");
+	
+	fnletraE();
+	fnletraL();
+	fnletraY();
+	fnletraD();
+	fnletraE();
+	
+	return 0;
+}
---
+++ Tarea 21/Nombre.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+void appInfoData(char *titulo,char *fecha)
+{
+  printf("----------------------------------------------------------------\n");
+  printf("_\n");
+  printf("_       %s\n", titulo);
+  printf("_       Elyde Montserratt Lopez Angel \n");
+  printf("_       %s\n", fecha);
+  printf("_\n");
+  printf("----------------------------------------------------------------\n\n");
+}
+
+void fnletraE(){
+		printf("E E E E \n");
+		printf("E       \n");
+		printf("E E E   \n");
+		printf("E       \n");
+		printf("E E E E \n\n");
+}
+
+void fnletraL(){
+		printf("L      \n");
+		printf("L      \n");
+		printf("L      \n");
+		printf("L      \n");
+		printf("L L L L\n\n");
+}
+
+void fnletraY(){
+		printf("Y   Y \n");
+		printf(" Y Y  \n");
+		printf("  Y   \n");
+		printf("  Y   \n");
+		printf("  Y   \n\n");
+}
+
+void fnletraD(){
+		printf("D D D  \n");
+		printf("D     D\n");
+		printf("D     D\n");
+		printf("D     D\n");
+		printf("D D D  \n\n");
+}
